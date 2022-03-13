9d61db07c1fd32271db9963478e719d3cf2d6c15
PawelC <pawel.cichocki.89@gmail.com>
2022-03-13 09:06:46 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        //test
         LoadData ld = new LoadData("data_test/");
     }
 }
