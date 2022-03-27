ba1618743ca64e1055188a55b48ba466fa5ca650
PawelC <pawel.cichocki.89@gmail.com>
2022-03-27 12:35:32 +0200

    Kalkulacja Charakterystyk od 3-10.

--- src/calculations/CharacteristicsExtraction.java
+++ src/calculations/CharacteristicsExtraction.java
@@ -3,6 +3,10 @@ package calculations;
 import prepare_data.Article;
 import prepare_data.Characteristic;
 
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public final class CharacteristicsExtraction {
     private CharacteristicsExtraction() {
 
@@ -18,42 +22,132 @@ public final class CharacteristicsExtraction {
         return calc;
     }
 
-    public static Characteristic c3 (String article){
-        Characteristic calc = null;
-        return calc;
+    public static Characteristic c3 (Article article){
+        return new Characteristic("c3", (double) article.body.length());
     }
 
-    public static Characteristic c4 (String article){
-        Characteristic calc = null;
-        return calc;
+    public static Characteristic c4 (Article article){
+        int sum = 0;
+        for (String word: article.words) {
+                sum += word.length();
+        }
+        return  new Characteristic("c4", (double) sum / (double) article.words.size());
     }
 
-    public static Characteristic c5 (String article){
-        Characteristic calc = null;
-        return calc;
+    public static Characteristic c5 (Article article){
+        List<String> distinctElements =  article.words.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        return new Characteristic("c5", (double) distinctElements.size());
     }
 
-    public static Characteristic c6 (String article){
-        Characteristic calc = null;
-        return calc;
+    public static Characteristic c6 (Article article){
+        int upperCase = 0;
+        for (String word: article.words) {
+            if(Character.isUpperCase(word.charAt(0))){
+                upperCase++;
+            }
+        }
+        return  new Characteristic("c6", (double) upperCase);
     }
 
-    public static Characteristic c7 (String article){
-        Characteristic calc = null;
-        return calc;
+    public static Characteristic c7 (Article article){
+        int longWords = 0;
+        for (String word: article.words) {
+            if(word.length()<3){
+                longWords++;
+            }
+        }
+        return  new Characteristic("c7", (double) longWords);
     }
 
     public static Characteristic c8 (Article article){
         return new Characteristic("c8", (double) article.words.size());
     }
 
-    public static Characteristic c9 (String article){
-        Characteristic calc = null;
-        return calc;
+    public static Characteristic c9 (Article article){
+        int longWords = 0;
+        for (String word: article.words) {
+            if(word.length()>8){
+                longWords++;
+            }
+        }
+        return  new Characteristic("c9", (double) longWords);
     }
 
-    public static Characteristic c10 (String article){
-        Characteristic calc = null;
-        return calc;
+    public static Characteristic c10 (Article article){
+        int upperCase = 0;
+        List<String> upperCaseChar = new ArrayList<>();
+        for (String word: article.words) {
+            if(Character.isUpperCase(word.charAt(0))){
+                upperCaseChar.add(String.valueOf(word.charAt(0)));
+            }
+        }
+        //unikalne litery
+        Map<String,Long> collect = upperCaseChar.stream()
+                .collect( Collectors.groupingBy( Function.identity(), Collectors.counting() ));
+
+        //sortowanie
+        LinkedHashMap<String, Long> countByWordSorted = collect.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (v1, v2) -> {
+                            throw new IllegalStateException();
+                        },
+                        LinkedHashMap::new
+                ));
+        return  new Characteristic("c10", getFirstKey(countByWordSorted) + " " + getFirstValue(countByWordSorted));
+//        return  new Characteristic("c10", getLastKey(countByWordSorted) + " " + getLastValue(countByWordSorted));
+    }
+
+    private static String getLastKey(LinkedHashMap<String, Long> lhm){
+        int count = 1;
+        for (Map.Entry<String, Long> it : lhm.entrySet()) {
+            if (count == lhm.size()) {
+                return it.getKey();
+            }
+            count++;
+        }
+        return "";
+    }
+
+    private static Long getLastValue(LinkedHashMap<String, Long> lhm){
+        int count = 1;
+        for (Map.Entry<String, Long> it : lhm.entrySet()) {
+            if (count == lhm.size()) {
+                return it.getValue();
+            }
+            count++;
+        }
+        return Long.valueOf(0);
+    }
+
+    // getFirst() method to get first element from
+    // java LinkedHashMap
+    private static String getFirstKey(LinkedHashMap<String, Long> lhm){
+        int count = 1;
+        for (Map.Entry<String, Long> it :
+                lhm.entrySet()) {
+            if (count == 1) {
+                return it.getKey();
+            }
+            count++;
+        }
+        return "";
+    }
+
+    private static Long getFirstValue(LinkedHashMap<String, Long> lhm){
+        int count = 1;
+        for (Map.Entry<String, Long> it :
+                lhm.entrySet()) {
+            if (count == 1) {
+                return it.getValue();
+            }
+            count++;
+        }
+        return Long.valueOf(0);
     }
 }
--- src/calculations/DocumentsClassification.java
+++ src/calculations/DocumentsClassification.java
@@ -22,16 +22,16 @@ public class DocumentsClassification{
     }
     public void calculateCharacteristics() {
         for (Article article: articles) {
-//            article.characteristics.add(CharacteristicsExtraction.c1(article.body));
-//            article.characteristics.add(CharacteristicsExtraction.c2(article.body));
-//            article.characteristics.add(CharacteristicsExtraction.c3(article.body));
-//            article.characteristics.add(CharacteristicsExtraction.c4(article.body));
-//            article.characteristics.add(CharacteristicsExtraction.c5(article.body));
-//            article.characteristics.add(CharacteristicsExtraction.c6(article.body));
-//            article.characteristics.add(CharacteristicsExtraction.c7(article.body));
-            article.characteristics.add(CharacteristicsExtraction.c8(article));
-//            article.characteristics.add(CharacteristicsExtraction.c9(article.body));
-//            article.characteristics.add(CharacteristicsExtraction.c10(article.body));
+//            article.characteristics.add(CharacteristicsExtraction.c1(article.body));//1. Najwięcej wyrazów w pierwszych 5 zdaniach.
+//            article.characteristics.add(CharacteristicsExtraction.c2(article.body));//2. Stosunek liczby wystąpień słów kluczowych do długości tekstu.
+            article.characteristics.add(CharacteristicsExtraction.c3(article));//3. Długość artykułu.
+            article.characteristics.add(CharacteristicsExtraction.c4(article));//4. Średnia dlugość słowa.
+            article.characteristics.add(CharacteristicsExtraction.c5(article));//5. Liczba unikalnych słów.
+            article.characteristics.add(CharacteristicsExtraction.c6(article));//6. Liczba słów zaczynająca się dużą literą.
+            article.characteristics.add(CharacteristicsExtraction.c7(article));//7. Liczba słów nie przekraczająca 3 znaków.
+            article.characteristics.add(CharacteristicsExtraction.c8(article));//8. Liczba słów.
+            article.characteristics.add(CharacteristicsExtraction.c9(article));//9. Liczba słów dłuższych niż 8 znaków.
+            article.characteristics.add(CharacteristicsExtraction.c10(article));//10. Litera na którą zaczyna się najwięcej słów z wielkiej litery
         }
     }
 }
