bdbc2cf5fb73c886a5cd79f3c4d95a6b209a5c7a
balnabu <przemek_lis@onet.eu>
2023-03-29 22:05:05 +0200

    added characteristics structure, maping function and enum

---
+++ src/calculations/CharacteristicsEnum.java
@@ -0,0 +1,24 @@
+package calculations;
+
+public enum CharacteristicsEnum {
+    C1("c1"),
+    C2("c2"),
+    C3("c3"),
+    C4("c4"),
+    C5("c5"),
+    C6("c6"),
+    C7("c7"),
+    C8("c8"),
+    C9("c9"),
+    C10("c10");
+
+    private final String value;
+
+    CharacteristicsEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
